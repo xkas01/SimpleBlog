6c4ebc99a49f509c2c350886f8d30aa4616cb266
Samandar <xkas2001@gmail.com>
2021-07-16 23:30:44 +0500

    posts controller in update action wrote

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -87,12 +87,26 @@ class PostsController extends Controller
      * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param string $slug
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, string $slug)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Post::where('slug', $slug)
+            ->update([
+                'title' => $request->input('title'),
+                'description' => $request->input('description'),
+                'slug' => SlugService::createSlug(Post::class, 'slug', $request->title),
+                'user_id' => auth()->user()->id
+            ]);
+
+        return redirect('/blog')
+            ->with('message', 'Your post has been updated!');
     }
 
     /**
--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -45,19 +45,6 @@
                 {{ $post->description }}
             </textarea>
 
-            <div class="bg-gray-50 pt-15">
-                <label
-                    class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
-                    <span class="mt-2 text-base leading-normal">
-                        Select a file
-                    </span>
-                    <input
-                        type="file"
-                        name="image"
-                        class="hidden">
-                </label>
-            </div>
-
             <button
                 type="submit"
                 class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
